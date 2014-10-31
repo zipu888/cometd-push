2effc06366366172b49be2672e4c79589115c13d
zipu888 <zipu405@126.com>
2014-10-31 17:08:57 +0800

    init commit

---
+++ src/main/java/sb/HelloService.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sb;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import org.cometd.bayeux.Message;
+import org.cometd.bayeux.server.BayeuxServer;
+import org.cometd.bayeux.server.ServerSession;
+import org.cometd.server.AbstractService;
+
+public class HelloService extends AbstractService
+{
+    public HelloService(BayeuxServer bayeux)
+    {
+        super(bayeux, "hello");
+        addService("/service/hello", "processHello");
+    }
+
+    /**
+     * 客户端 发送一条消息，服务端就会调用这个方法。当然服务端也可以主动调用这个方法。
+     * @param remote
+     * @param message
+     * @author ChenHua
+     * create on 2012-10-31  下午3:17:51
+     */
+    public void processHello(ServerSession remote, Message message)
+    {
+        Map<String, Object> input = message.getDataAsMap();
+        String name = (String)input.get("name");
+        Map<String, Object> output = new HashMap<String, Object>();
+        output.put("greeting", "Hello, " + name);
+        remote.deliver(getServerSession(), "/hello", output, null);
+    }
+}
